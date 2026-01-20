0af31c2ad6a6461cf64c89c65f9fe9918272302c
Hawkie <ahribestplayer@gmail.com>
2026-01-20 12:14:57 +0200

    feat: update stats

--- src/components/QueueStats.tsx
+++ src/components/QueueStats.tsx
@@ -8,29 +8,14 @@ type Props = {
 const QueueStats: FC<Props> = ({ without, withValue }) => {
 	return (
 		<div className='rounded-2xl bg-slate-900/70 p-4 shadow-lg'>
-			<h2 className='mb-2 text-sm font-semibold'>Підрахунок на добу</h2>
-
-			<div className='grid grid-cols-2 gap-3'>
-				<div>
-					<label className='mb-1 block text-xs text-slate-300'>
-						Без світла (год)
-					</label>
-					<input
-						readOnly
-						value={without}
-						className='w-full rounded-xl border border-slate-700 bg-slate-950/70 px-3 py-2 text-sm outline-none'
-					/>
+			<div className='grid grid-cols-2 gap-3 text-xs'>
+				<div className='flex gap-3 justify-start items-center'>
+					<span className='text-slate-400'>Без світла (год)</span>
+					<span className='text-red-600'>{without}</span>
 				</div>
-
-				<div>
-					<label className='mb-1 block text-xs text-slate-300'>
-						Зі світлом (год)
-					</label>
-					<input
-						readOnly
-						value={withValue}
-						className='w-full rounded-xl border border-slate-700 bg-slate-950/70 px-3 py-2 text-sm outline-none'
-					/>
+				<div className='flex gap-3 justify-start items-center'>
+					<span className='text-slate-400'>Зі світлом (год)</span>
+					<span className='text-green-400'>{withValue}</span>
 				</div>
 			</div>
 		</div>
